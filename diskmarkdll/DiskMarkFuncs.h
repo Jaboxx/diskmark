a3efa10c0895100ab2434014330c8d4bb2297828
fenhuo <fenhuo@DESKTOP-HT5KOUL>
2017-11-27 19:29:44 +0800

    添加项目文件。

---
+++ diskmarkdll/DiskMarkFuncs.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifdef MATHFUNCSDLL_EXPORTS
+#define MATHFUNCSDLL_API __declspec(dllexport) 
+#else
+#define MATHFUNCSDLL_API __declspec(dllimport) 
+#endif
+namespace diskmark 
+{
+
+
+
+
+
+
+
+
+
+
+}
